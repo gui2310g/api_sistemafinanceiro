d93fcaa962f02fe0086f2a2a0e023f208fe959ca
gui2310g <fontesguilherme57@gmail.com>
2025-01-25 20:14:38 -0300

    first commit

---
+++ src/test/java/com/example/api_sistemafinanceiro/gui/GuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_sistemafinanceiro.gui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
